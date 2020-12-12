d9f5916e1a5d7adc484b137db42bed5839b20f9f
mkdv00 <maxim.cudaew@gmail.com>
2020-12-12 12:22:18 +0300

    Added a 1 day of bootcamp. Band name Generator

---
+++ Day_1/band_name_generator.py
@@ -0,0 +1,9 @@
+def main():
+    print("Welcome to the Band Name Generator.")
+    street = input("What's name of the city you grew up in?\n")
+    pet = input("What's your pet's name?\n")
+    print("Your band name could be " + street + " " + pet)
+
+
+if __name__ == "__main__":
+    main
