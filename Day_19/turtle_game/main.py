c424f3a1730dba273a9c54868769f8350313fd12
unknown <maxim.cudaew@gmail.com>
2021-01-18 09:22:13 +0300

    Added 23 day of bootcamp. Created the turtle crossing game.

--- Day_19/turtle_game/main.py
+++ Day_19/turtle_game/main.py
@@ -7,6 +7,7 @@ def main():
     screen.setup(width=500, height=400)
     is_race_on = False
     user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+
     colors = ["red", "orange", "yellow", "green", "blue", "purple"]
     y_positions = [-70, -40, - 10, 20, 50, 80]
     all_turtles = []
